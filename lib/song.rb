4196966d18a544e5012c5d868efe55e553bfb322
Dan Foley <dan@dan-foley.com>
2020-01-30 19:20:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,10 @@ class Song
     self.all.find{|song| song.name == name}
   end
   
-  def self.find_or_create_by_name
-    
+  def self.find_or_create_by_name(name)
+    if !self.find_by_name(name)
+      
+    end
   end
   
 end
